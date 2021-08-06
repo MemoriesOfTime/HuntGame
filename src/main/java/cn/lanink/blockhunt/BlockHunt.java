2dcfcf6930114eace525cd948726451fc0e2c4bd
lt_name <572413378@qq.com>
2021-08-06 21:07:42 +0800

    Stuff

--- src/main/java/cn/lanink/blockhunt/BlockHunt.java
+++ src/main/java/cn/lanink/blockhunt/BlockHunt.java
@@ -27,7 +27,7 @@ import java.util.*;
  */
 public class BlockHunt extends PluginBase {
 
-    public static final String VERSION = "0.0.4 git-4a63424";
+    public static final String VERSION = "?";
 
     private static BlockHunt BLOCK_HUNT;
 
