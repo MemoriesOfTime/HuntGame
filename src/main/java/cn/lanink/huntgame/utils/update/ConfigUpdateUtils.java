6d9aba21288cb479624ef2995713824807c9db58
LT_Name <572413378@qq.com>
2023-06-11 20:37:24 +0800

    实现配置自动更新

--- src/main/java/cn/lanink/huntgame/utils/update/ConfigUpdateUtils.java
+++ src/main/java/cn/lanink/huntgame/utils/update/ConfigUpdateUtils.java
@@ -6,7 +6,7 @@ import cn.nukkit.utils.Config;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.LinkedHashMap;
+import java.util.*;
 
 /**
  * @author LT_Name
@@ -16,6 +16,27 @@ public class ConfigUpdateUtils {
 
     public static void updateConfig() {
         update1_X_X_To_1_2_2();
+        update1_2_2_To_1_3_0();
+    }
+
+    private static void update1_2_2_To_1_3_0() {
+        Config config = new Config(HuntGame.getInstance().getDataFolder() + "/config.yml", Config.YAML);
+        if (VersionUtils.compareVersion(config.getString("ConfigVersion", "1.0.0"), "1.3.0") >= 0) {
+            return;
+        }
+
+        config.set("ConfigVersion", "1.3.0");
+
+        if (!config.exists("victoryRewardCmd")) {
+            HashMap<String, List<String>> map = new HashMap<>();
+            map.put("0", Collections.singletonList("tell \"@p\" 参与奖！&con"));
+            map.put("60", Collections.singletonList("tell \"@p\" 表现良好！&con"));
+            map.put("121", Arrays.asList("tell \"@p\" 表现出色！&con", "me I am the best"));
+
+            config.set("victoryRewardCmd", map);
+        }
+
+        config.save();
     }
 
     private static void update1_X_X_To_1_2_2() {
